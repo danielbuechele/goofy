44576fbc251ec5dfc80e314b978c407a071cbc3e
Daniel Büchele <daniel@buechele.cc>
2014-11-29 21:51:54 -0800

    Initial Commit

---
+++ Goofy/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Goofy
+//
+//  Created by Daniel Büchele on 11/29/14.
+//  Copyright (c) 2014 Daniel Büchele. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
